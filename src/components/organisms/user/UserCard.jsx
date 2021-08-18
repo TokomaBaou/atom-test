cbccc8cde7267518fe36a0b9102d6f9a18ae5006
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-18 15:18:29 +0900

    userIconWithNameの作成

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,13 +1,12 @@
 import styled from "styled-components";
 import { Card } from "../../atoms/card/Card";
-
+import { UserIconWithName } from "../../atoms/molecules/user/UserIconWithName";
 export const UserCard = (props) => {
   const { user } = props;
 
   return (
     <Card>
-      <img height={160} width={160} src={user.image} alt={user.name} />
-      <p>{user.name}</p>
+      <UserIconWithName image={user.image} name={user.name} />
       <SDl>
         <dt>メール</dt>
         <dd>{user.email}</dd>
@@ -31,5 +30,7 @@ const SDl = styled.dl`
   dd {
     padding-left: 32px;
     padding-bottom: 8px;
+    /**折り返し*/
+    overflow-wrap: break-word;
   }
 `;
