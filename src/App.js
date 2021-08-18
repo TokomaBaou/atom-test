36a0f40292c2d153e782cad33c7e64d41af69022
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-18 13:37:52 +0900

    organismsの作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./styles.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/atoms/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput></SearchInput>
+      <UserCard />
     </div>
   );
 }
---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>fdsa@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-9999-8888</dd>
+        <dt>会社名</dt>
+        <dd>株式会社mfdsa</dd>
+        <dt>WEB</dt>
+        <dd>mfdsa.com</dd>
+      </dl>
+    </div>
+  );
+};
